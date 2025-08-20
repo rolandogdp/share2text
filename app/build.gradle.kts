5d48a335f26956231e8e7116bec8da873cfeac70
rolandogdp <rolandt@graved.ch>
2025-08-21 00:24:53 +0200

    more fixing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
@@ -106,9 +107,6 @@ dependencies {
     // OkHttp + logging
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
-
-    // Accompanist (optional)
-    implementation("com.google.accompanist:accompanist-permissions:0.35.3-beta")
 
     // Kotlin serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
